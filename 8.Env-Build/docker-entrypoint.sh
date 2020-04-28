55b0d6658edb45f5e8f8462e00366e0e60a39c18
Ebby <ebby.dd@gmail.com>
2020-04-28 18:25:11 +0800

    update vscode entry
    Signed-off-by: Ebby <ebby.dd@gmail.com>

--- 8.Env-Build/docker-entrypoint.sh
+++ 8.Env-Build/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ $# -eq 0 ]
   then
     jupyter lab --ip=0.0.0.0 --NotebookApp.token='CXY2019!!!!' --allow-root --no-browser --notebook-dir=/code &> /dev/null &
-    code-server-3.1.1-linux-x86_64/code-server --allow-http --password 'CXY2019!!!!' --data-dir /data /code
+    code-server-3.1.1-linux-x86_64/code-server --host 0.0.0.0 --port 8443 --auth none --user-data-dir /data --disable-ssh
   else
     exec "$@"
 fi
